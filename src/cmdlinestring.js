2cb8c518658196975c583ea3d931fb3001f9b152
Vincent van Pelt <vincent.vanpelt@hotmail.com>
2018-11-04 11:46:30 +0100

    Fix export when input is empty

--- src/cmdlinestring.js
+++ src/cmdlinestring.js
@@ -6,11 +6,15 @@ function copytoclipboard() {
 function exporttofile() {
   // https://stackoverflow.com/questions/14964035/how-to-export-javascript-array-info-to-csv-on-client-side
 
-  var encodedUri = encodeURI("data:text," + $("#cmdline").val());
-  var link = document.createElement("a");
-  link.setAttribute("href", encodedUri);
-  link.setAttribute("download", "cmdline-string.txt");
-  document.body.appendChild(link); // Required for FF
+  var cmdline = $("#cmdline").val()
 
-  link.click();
+  if(cmdline != "") {
+    var encodedUri = encodeURI("data:text," + cmdline);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "cmdline-string.txt");
+    document.body.appendChild(link); // Required for FF
+
+    link.click();
+  }
 }
